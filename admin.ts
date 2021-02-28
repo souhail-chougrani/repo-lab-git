6a83be8bf89c67b1a5baa8c47b77593b30b1e367
Souhail <chougranisouhail@gmail.com>
2021-02-28 15:54:09 +0100

    first commit with cm alias

--- admin.ts
+++ admin.ts
@@ -1,1 +1,4 @@
-const admin ="souhail"
+const admin = "souhail"
+
+// git config alias.[short cut] [command] ==> (i.e) git config alias.cm "commit -m" 
+//                                        ==> (i.e) git config alias  status 
