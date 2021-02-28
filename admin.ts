8a34648e907efdc6f94bf096367e193e8aac8601
Souhail <chougranisouhail@gmail.com>
2021-02-28 16:24:17 +0100

    alias for pretty history

--- admin.ts
+++ admin.ts
@@ -2,3 +2,6 @@ const admin = "souhail"
 
 // git config alias.[short cut] [command] ==> (i.e) git config alias.cm "commit -m" 
 //                                        ==> (i.e) git config alias  status 
+
+
+//for pretty log history we can use this commande ==> git log --pretty='%Cred%h%Creset | %Cgreen%d%Creset %s (%C(yellow)%cr%Creset) [%C(cyan)%an%Creset]'
